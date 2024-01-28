3b76d8ca6b5f9c8145e66e1ee4be1821b915ca66
flashyflash369 <137924111+flashyflash369@users.noreply.github.com>
2024-01-28 01:57:04 +0100

    init-commit

---
+++ Assets/NewInputSystem/Scripts/InputDebuggerActionExample.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputDebuggerActionExample : MonoBehaviour
+{
+
+    public InputAction exampleAction;
+
+    private void OnEnable()
+    {
+        exampleAction.Enable();
+    }
+}
---
+++ Assets/NewInputSystem/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    [SerializeField] private float speed = 1f;
+
+
+    void Start()
+    {
+        rb =  this.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+}
